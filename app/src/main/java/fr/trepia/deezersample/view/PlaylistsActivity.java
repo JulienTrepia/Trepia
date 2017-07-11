171fa4c28e1c77c8aea78183e8e5d1ac70065a3c
JulienTrepia <j.robert@trepia.fr>
2017-07-11 04:09:45 +0200

    Rename layout files

--- app/src/main/java/fr/trepia/deezersample/view/PlaylistsActivity.java
+++ app/src/main/java/fr/trepia/deezersample/view/PlaylistsActivity.java
@@ -42,7 +42,7 @@ public class PlaylistsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_playlists);
 
         Fresco.initialize(this);
         findViews();
--- app/src/main/java/fr/trepia/deezersample/view/TracksActivity.java
+++ app/src/main/java/fr/trepia/deezersample/view/TracksActivity.java
@@ -50,7 +50,7 @@ public class TracksActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_playlist);
+        setContentView(R.layout.activity_tracks);
 
         findViews();
 
